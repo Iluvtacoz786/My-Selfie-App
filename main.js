707bb5f3b6a2f7c3147a676b58c3d163c17bae98
Zoya Rana <74219818+Iluvtacoz786@users.noreply.github.com>
2021-04-22 17:53:25 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -8,4 +8,11 @@ recognition.onresult=function (event){
     console.log(event);
     content=event.results[0][0].transcript;
     document.getElementById("text_area").value=content;
+    speak();
+}
+function speak(){
+var synth=window.speechSynthesis;
+speak_data=document.getElementById("text_area").value;
+var utter_this=new SpeechSynthesisUtterance(speak_data);
+synth.speak(utter_this);
 }
