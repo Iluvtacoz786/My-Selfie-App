845b5bee7c247f6f83eff46d56ccc934b8699328
Zoya Rana <74219818+Iluvtacoz786@users.noreply.github.com>
2021-04-27 18:52:35 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -8,11 +8,36 @@ recognition.onresult=function (event){
     console.log(event);
     content=event.results[0][0].transcript;
     document.getElementById("text_area").value=content;
-    speak();
+    if (content=="take my selfie"){
+        speak();
+        Webcam.attach( '#camera' );
+        setTimeout(function(){
+            take_selfie()
+        },5000);
+    }
 }
 function speak(){
 var synth=window.speechSynthesis;
-speak_data=document.getElementById("text_area").value;
+speak_data="taking your selfie in five seconds"
 var utter_this=new SpeechSynthesisUtterance(speak_data);
 synth.speak(utter_this);
+
 }
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+  });
+  function take_selfie(){
+      Webcam.snap(function(data_URI){
+          document.getElementById("result").innerHTML="<img src='"+data_URI+"' id='result_img'>"
+      })
+  }
+  function save(){
+      link=document.getElementById("link");
+      img=document.getElementById("result_img").src;
+      link.href=img;
+      link.click()
+  }
+  
